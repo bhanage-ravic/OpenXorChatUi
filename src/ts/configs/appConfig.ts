be12b0b5dfb9217772322e4660888d7f69dcd237
Abhijeet Sonaje <sonaje.abhijeet@gmail.com>
2018-05-09 16:54:56 +0530

    initialized

---
+++ src/ts/configs/appConfig.ts
@@ -0,0 +1,17 @@
+export let appConfig: collab.config.IAppConfig = <any>{
+    server_url: "",
+    user_details: {
+
+    },
+    session_header: {},
+    user_details_url: "",
+    env: "",
+    firebase_config_for_web: {
+        apiKey: "AIzaSyAmJ-Mfxx5E7x4vHc227C5B0IAAoOd7agU",
+        authDomain: "openxorchat.firebaseapp.com",
+        databaseURL: "https://openxorchat.firebaseio.com",
+        projectId: "openxorchat",
+        storageBucket: "openxorchat.appspot.com",
+        messagingSenderId: "150881070054"
+    }
+};
